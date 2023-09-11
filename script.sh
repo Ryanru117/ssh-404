8eab1405386e7a3d294ffbd76d93ac8db0dd7b00
Ryanru117 <144712115+Ryanru117@users.noreply.github.com>
2023-09-12 00:15:13 +0630

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,19 +22,19 @@ echo "
 <h3><font color='magenta'>--- ۩ PREMIUM SERVER ۩ ---
 </font></h3>
 
-<h3><font color='green'>--- ۩ SERVER BY ④ ⍤⃝🌻④ ۩ ---
+<h3><font color='green'>--- ۩ SERVER BY Ryan Ru ۩ ---
 </font></h3>
 
-<h4><font color='cyan'>Telegram Channel >> https://t.me/Pmttg
+<h4><font color='cyan'>Ngal Ko Pl Cx Tl🫶🏻
 </font></h4>
 
-<h4><font color='blue'>Telegram Channel >> https://t.me/nkka_404
+<h4><font color='blue'>
 </font></h4>
 
 <h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
 </font></h3>" | tee /etc/ssh/gcp_404 >/dev/null
-useradd "404" --shell=/bin/false -M
-echo "404:404" | chpasswd
+useradd "ryan" --shell=/bin/false -M
+echo "ryan:ryan" | chpasswd
 
 echo ""
 
